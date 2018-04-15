6106a0530bea56dc6940fc16e24aac77e6152dab
Dhaval <dashah@gmail.com>
2018-04-15 16:56:01 -0400

    heroku fun

--- show_image.py
+++ show_image.py
@@ -8,8 +8,9 @@ def source():
   try:
     with open('state/after_login.png',"rb") as i:
       encoded_string = base64.b64encode(i.read())
-    html = '<img  src="data:image/jpeg;base64;+encoded_string">'
-  except:
+      html = '<img src="data:image/png;base64;+encoded_string">'
+  except e:
+    print(e)
     html = 'Hello World!'
   return html
 
