897f834acb7f6e38cef8b763d49f321282149680
Dhaval <dashah@gmail.com>
2018-04-16 17:01:55 -0400

    db fun

--- show_image.py
+++ show_image.py
@@ -14,8 +14,7 @@ def source():
     try:
         db = client.get_default_database().settings
         screenshot = db.find_one({'type':'screenshot'})
-        print(screenshot)
-        image_file = BytesIO(pickle.loads(screenshot.value))
+        image_file = BytesIO(pickle.loads(screenshot['value']))
         send_file(image_file, attachment_filename='logo.png', mimetype='image/png')
     except Exception as exc:
         print(str(exc))
