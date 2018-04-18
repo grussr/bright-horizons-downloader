2af956ee25f02eb6de86a8f2503ae5bb02d788c4
Dhaval <dashah@gmail.com>
2018-04-17 20:09:29 -0400

    add directory listing

--- show_image.py
+++ show_image.py
@@ -2,6 +2,7 @@ import os
 from io import BytesIO
 from flask import Flask
 from flask import send_file
+from flask import abort
 from pymongo import MongoClient
 import pickle
 
@@ -21,5 +22,25 @@ def source():
         
     return 'Hello World!'
 
+@app.route('/list/', defaults={'req_path': ''})
+@app.route('/list/<path:req_path>')
+def dir_listing(req_path):
+    BASE_DIR = '/app/img'
+
+    # Joining the base and the requested path
+    abs_path = os.path.join(BASE_DIR, req_path)
+
+    # Return 404 if path doesn't exist
+    if not os.path.exists(abs_path):
+        return abort(404)
+
+    # Check if path is a file and serve
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)
+
+    # Show directory contents
+    files = os.listdir(abs_path)
+    return render_template('files.html', files=files)
+    
 if __name__ == '__main__':
     app.run()
